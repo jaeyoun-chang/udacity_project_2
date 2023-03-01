7e56b0e135c58ea40745db68d10428651e4738d4
michael <jaeyoun.chang@icloud.com>
2023-03-02 05:11:18 +0900

    process_data.py correction on sql

--- data/process_data.py
+++ data/process_data.py
@@ -51,7 +51,7 @@ def save_data(df, database_filename):
     Function to save df as SQL
     '''
     engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql(database_filename, engine, index=False)
+    df.to_sql(database_filename, engine, index=False, if_exists = 'replace')
 
 def main():
     if len(sys.argv) == 4:
